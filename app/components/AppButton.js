c44b485fbac0cc77a0a7abb206653a863ecbf8ab
jakesjacob <33692879+jakesjacob@users.noreply.github.com>
2021-01-01 22:07:37 +0000

    Completed the Form Components

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, } from 'react-native';
-import colors from '../config/colors';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import colors from "../config/colors";
 
-
-function AppButton({title, onPress, color = "KUP1", textColor = "KUP2"}) {
-    return (
-        <TouchableOpacity style={[styles.button, {backgroundColor: colors[color]} ]} onPress={onPress}>
-            <Text style={[styles.text, {color: colors[textColor]}]}>{title}</Text>
-        </TouchableOpacity>
-    );
+function AppButton({ title, onPress, color = "KUP2", textColor = "white" }) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={[styles.text, { color: colors[textColor] }]}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: colors.KUP2,
-        borderRadius: 25,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 15,
-        width: "100%",
-        
-    },
-    text: {
-        color: colors.KUP1,
-        fontSize: 18,
-        textTransform: "uppercase",
-    },
-    
-})
+  button: {
+    backgroundColor: colors.KUP1,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    width: "100%",
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    textTransform: "uppercase",
+  },
+});
 
 export default AppButton;
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,77 +1,66 @@
-import React from 'react';
-import { Image, StyleSheet } from 'react-native';
+import React from "react";
+import { Image, StyleSheet } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 
-import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
 import Screen from "../components/Screen";
-import AppText from '../components/AppText/AppText';
-import ErrorMessage from '../components/ErrorMessage';
+import AppText from "../components/AppText/AppText";
+import ErrorMessage from "../components/ErrorMessage";
+import AppFormField from "../components/AppFormField";
+import SubmitButton from "../components/SubmitButton";
+import AppForm from "../components/AppForm";
 
 const validationSchema = Yup.object().shape({
-    email: Yup.string().required().email().label("Email"),
-    password: Yup.string().required().min(4).label("Password"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
 });
 
 function LoginScreen(props) {
-
-    return (
-        <Screen style={styles.container}>
-            <Image 
-                style={styles.logo}
-                source={require("../assets/five-dice.png")} />
-            <Formik
-                initialValues={{ email: "", password: ""}}
-                onSubmit={values => console.log(values)}
-                validationSchema={validationSchema}
-            >
-                {({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
-                    <>
-                        <AppTextInput 
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            icon="email"
-                            placeholder="Email"
-                            keyboardTypre="email-address"
-                            textContentType="emailAddress"
-                            onChangeText={handleChange("email")}
-                            onBlur={() => setFieldTouched("email")}
-                        />
-                        <ErrorMessage error={errors.email} visible={touched.email} />
-                        <AppTextInput 
-                            autoCapitalize="none"
-                            autoCorrect={false}
-                            icon="lock"
-                            placeholder="Password"
-                            textContentType="password"
-                            secureTextEntry
-                            onChangeText={handleChange("password")}
-                            onBlur={() => setFieldTouched("password")}
-                        />
-                        <ErrorMessage error={errors.password} visible={touched.password} />
-                        <AppButton 
-                            title="Login"
-                            onPress={handleSubmit}
-                        />
-                    </>
-                ) }
-            </Formik>
-        </Screen>
-    );
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/five-dice.png")} />
+      <AppForm
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          placeholder="Email"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          name="email"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          placeholder="Password"
+          textContentType="password"
+          secureTextEntry
+          name="password"
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-    },
-    logo: {
-        width: 80,
-        height: 80,
-        alignSelf: "center",
-        marginTop: 50,
-        marginBottom: 20,
-    },
-})
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
 
 export default LoginScreen;
